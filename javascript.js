755d725d87c189ec2a397a44920d342493c70981
Henrik <henrik.westerlund@skola.taby.se>
2023-05-11 15:00:00 +0200

    hamburgermeny

--- javascript.js
+++ javascript.js
@@ -1,47 +1,19 @@
 /* hamburgar meny: */
 
-// hide nav open btn when the nav is open, adding/removing open classes to nav and content
-var navOpenBtn = document.querySelector(".nav-open-btn");
-var navCloseBtn = document.querySelector(".nav__close");
-var nav = document.querySelector(".nav");
-var pageContent = document.querySelector(".page__content");
-var navList = document.querySelector(".nav__list");
-var page = document.querySelector(".page");
-// open nav
-navOpenBtn.addEventListener("click", function () {
-  navOpenBtn.classList.add("js-hidden");
-  nav.classList.add("js-opened");
-  pageContent.classList.add("js-opened");
-});
-// close nav
-navCloseBtn.addEventListener("click", function () {
-  navOpenBtn.classList.remove("js-hidden");
-  nav.classList.remove("js-opened");
-  pageContent.classList.remove("js-opened");
-});
-// close navigation if click outside it
-page.addEventListener("click", function (e) {
-  var evTarget = e.target;
-  if (
-    //evTarget !== nav &&
-    nav.classList.contains("js-opened") &&
-    evTarget !== navOpenBtn &&
-    evTarget.parentNode !== navOpenBtn
-  ) {
-    console.log(navOpenBtn.firstChild);
-    navOpenBtn.classList.remove("js-hidden");
-    nav.classList.remove("js-opened");
-    pageContent.classList.remove("js-opened");
-  }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-//hide nav open btn when the nav is open, adding/removing open classes to nav and content
-var navOpenBtn = document.querySelector(".nav-open-btn");
-var navCloseBtn = document.querySelector(".nav__close");
-var nav = document.querySelector(".nav");
-var pageContent = document.querySelector(".page__content");
-var navList = document.querySelector(".nav__list");
-var page = document.querySelector(".page");
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
 
 function reveal() {
   var revealsLeft = document.querySelectorAll(".revealLeft");
@@ -85,7 +57,3 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
-
-//adding default demo classes
-nav.classList.add("nav--offcanvas-1");
-pageContent.classList.add("page__content--offcanvas-1");

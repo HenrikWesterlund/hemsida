83d443bf8851c68485102ece2a4389516f5f3413
Henrik <henrik.westerlund@skola.taby.se>
2023-05-11 13:32:15 +0200

    mycket

--- javascript.js
+++ javascript.js
@@ -44,50 +44,40 @@ var navList = document.querySelector(".nav__list");
 var page = document.querySelector(".page");
 
 function reveal() {
-  var reveals1 = document.querySelectorAll(".reveal1");
-  var reveals2 = document.querySelectorAll(".reveal2");
-  var reveals3 = document.querySelectorAll(".reveal3");
-  var reveals4 = document.querySelectorAll(".reveal4");
   var revealsLeft = document.querySelectorAll(".revealLeft");
   var revealsRight = document.querySelectorAll(".revealRight");
   var revealsUp = document.querySelectorAll(".revealUp");
-  var revealsRight2 = document.querySelectorAll(".revealRight2");
-  var revealsUp3 = document.querySelectorAll(".revealUp3");
 
-  for (var i = 0; i < reveals2.length; i++) {
+  for (var i = 0; i < revealsLeft.length; i++) {
     var windowHeight = window.innerHeight;
-    var elementTop2 = reveals2[i].getBoundingClientRect().top;
-    var elementVisible = 0;
-
-    if (elementTop2 < windowHeight - elementVisible) {
-      revealsRight2[i].classList.add("active");
-    } else {
-      revealsRight2[i].classList.remove("active");
-    }
-  }
-  for (var i = 0; i < reveals3.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop3 = reveals3[i].getBoundingClientRect().top;
-    var elementVisible = 0;
-
-    if (elementTop3 < windowHeight - elementVisible) {
-      revealsUp3[i].classList.add("active");
-    } else {
-      revealsUp3[i].classList.remove("active");
-    }
-  }
-  for (var i = 0; i < reveals1.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop1 = reveals1[i].getBoundingClientRect().top;
-    var elementVisible = 100;
+    var elementTop1 = revealsLeft[i].getBoundingClientRect().top;
+    var elementVisible = 50;
 
     if (elementTop1 < windowHeight - elementVisible) {
       revealsLeft[i].classList.add("active");
-      revealsRight[i].classList.add("active");
-      revealsUp[i].classList.add("active");
     } else {
       revealsLeft[i].classList.remove("active");
+    }
+  }
+  for (var i = 0; i < revealsRight.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop2 = revealsRight[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+
+    if (elementTop2 < windowHeight - elementVisible) {
+      revealsRight[i].classList.add("active");
+    } else {
       revealsRight[i].classList.remove("active");
+    }
+  }
+  for (var i = 0; i < revealsUp.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop3 = revealsUp[i].getBoundingClientRect().top;
+    var elementVisible = -100;
+
+    if (elementTop3 < windowHeight - elementVisible) {
+      revealsUp[i].classList.add("active");
+    } else {
       revealsUp[i].classList.remove("active");
     }
   }
